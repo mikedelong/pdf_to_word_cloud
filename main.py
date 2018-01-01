3ee584a87d67043e68ce715571b672da22ff7f98
Mike DeLong <mikedelong@outlook.com>
2018-01-01 12:36:15 -0500

    feature: logging, input folder, list of files in input folder.

---
+++ main.py
@@ -0,0 +1,21 @@
+import logging
+import textract
+
+import os
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+input_folder = './input/'
+
+for file_name in os.listdir(input_folder):
+    logger.debug(file_name)
+
+    
