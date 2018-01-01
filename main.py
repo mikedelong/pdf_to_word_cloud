60d66d3f661f0325eb184b4ffe41e266f18b7b59
Mike DeLong <mikedelong@outlook.com>
2018-01-01 16:56:32 -0500

    feature: added code to allow us to determine the shape and size of the output figure.

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ logger.debug('started')
 input_folder = './input/'
 output_folder = './output/'
 
+ignore_words = ['also', 'one']
+ignore_words = sorted(ignore_words)
+logger.debug('words to ignore: %s' % ignore_words)
 
 for file_name in os.listdir(input_folder):
     logger.debug('base input file name: %s' % file_name)
@@ -28,10 +31,13 @@ for file_name in os.listdir(input_folder):
     input_text = textract.process(input_file_name).lower()
     text = ' '.join(input_text.split('\n'))
 
-    text = text.replace('also', '')
+    for ignore_word in ignore_words:
+        text = text.replace(ignore_word, '')
 
     result = wordcloud.WordCloud().generate(text)
-    plt.imshow(result, interpolation='bilinear')
+    figure, axes = plt.subplots(figsize=(16, 9))
+    axes.imshow(result, interpolation='bilinear')
+    figure.tight_layout()
     plt.axis("off")
     output_file_name = file_name.replace('.pdf', '.jpg')
     full_output_filename = output_folder + output_file_name
