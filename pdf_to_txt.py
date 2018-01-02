3a8db1da83ef3d9215a75e9f0008109912d18449
Mike DeLong <mikedelong@outlook.com>
2018-01-02 16:50:42 -0500

    feature: added words and tokens to ignore, output filename.

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -13,6 +13,7 @@ logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
+punctuation_outliers = {'i.e.': 'ie', 'U.S.': 'US'}
 ignore_words = ['also', 'one']
 ignore_words = sorted(ignore_words)
 logger.debug('words to ignore: %s' % ignore_words)
@@ -25,3 +26,18 @@ for item in os.listdir(input_folder):
     logger.debug('relative input file name: %s' % input_file_name)
 
     text = textract.process(input_file_name)
+
+    for key, value in punctuation_outliers.iteritems():
+        text = text.replace(key, value)
+    for ignore_word in ignore_words:
+        text = text.replace(ignore_word, '')
+
+    for token in ['\n', '.', ',', '(', ')']:
+        text = text.replace(token, ' ')
+    text = text.split(' ')
+
+    output_file = item.replace('.pdf', '.txt')
+    full_output_filename = output_folder + output_file
+
+    for token in text:
+        logger.debug(token)
