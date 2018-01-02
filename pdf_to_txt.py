401bda80f06b095f4226cbb0d37a2c17349c0bbc
Mike DeLong <mikedelong@outlook.com>
2018-01-02 16:41:43 -0500

    feature: text extraction using textract; also, basic support for a list of words to ignore.

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+import textract
+
 # set up logging
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
 logger = logging.getLogger('main')
@@ -11,7 +13,15 @@ logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
+ignore_words = ['also', 'one']
+ignore_words = sorted(ignore_words)
+logger.debug('words to ignore: %s' % ignore_words)
+
 input_folder = './input/'
 output_folder = './output/'
 for item in os.listdir(input_folder):
-    logger.debug('%s contains file or subfolder %s' % (input_folder, item))
+    logger.debug('base input file name: %s' % item)
+    input_file_name = input_folder + item
+    logger.debug('relative input file name: %s' % input_file_name)
+
+    text = textract.process(input_file_name)
